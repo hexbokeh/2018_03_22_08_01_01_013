ec7ae8de4291ba0fc8531d9405435504176babd8
hexbokeh <jwallachstudent@gmail.com>
2018-03-28 21:28:24 -0400

    newCalibrate
    Beginning newCalibrate() function for bonus exercise.

--- calibrate.ino
+++ calibrate.ino
@@ -16,3 +16,17 @@ void calibrateValues()
     Serial.println(darkValue);
   }
 }
+
+// bonus: use just one button to establish light and dark extremes.
+// it will be the user's job to adjust the light values accordingly.
+void newCalibrate()
+{
+  // either button from before can be used; now they're performing the same function.
+  while (digitalRead(buttons[0]) || digitalRead(buttons[1])) // perhaps this could be an if statement instead.
+  {
+    int temp = analogRead(photoPin);
+    // light yields a higher analog value; darkness yields a lower analog value.
+    if (temp > lightValue) lightValue = temp; 
+  }
+}
+
